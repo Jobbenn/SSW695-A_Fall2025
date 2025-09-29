4d6c467752c8fca24aeca408d621a49640abe861
Jobbbenn <joseph.bender@gmail.com>
2025-09-29 18:49:09 -0500

    Supabase build for Healthbud

---
+++ HealthBud/App.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react'
+import { supabase } from './lib/supabase'
+import Auth from './components/Auth'
+import Account from './components/Account'
+import { Image } from 'expo-image'
+import { StyleSheet, Text, View } from 'react-native'
+import { Session } from '@supabase/supabase-js'
+
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.logo}>
+        <Image
+          source={require('./assets/HealthBud.png')} // For local images
+          contentPosition={"center"}
+          style={{ width: 200, height: 200 }}
+          contentFit="cover" // Adjusts how the image is resized to fit its container
+        />
+        <Text style={styles.titleText}>HealthBud</Text>
+      </View>
+      <View style={styles.login}>
+        {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#faedde'
+  },
+  logo: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 100,
+  },
+  login: {
+  },
+  titleText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});
