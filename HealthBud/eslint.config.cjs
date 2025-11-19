01d6849bc1c2384ad4c4db2540a025a21f16b1a0
JamesWu <jameswu6789@gmail.com>
2025-11-18 23:11:32 -0500

    fix: add eslint.config.cjs (ESLint v9 compatible CJS config)

---
+++ HealthBud/eslint.config.cjs
@@ -0,0 +1,21 @@
+const js = require("@eslint/js");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+
+module.exports = [
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+];
